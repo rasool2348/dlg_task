7c5015c234e8a37a05c35dab8146263f42cc1866
r.kakaei <r.kakaei@irisaco.com>
2023-12-10 14:50:00 +0330

    implementing user interface for signup page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss'],
+  standalone: true,
 })
 export class SignupComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
