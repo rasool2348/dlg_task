9ba0af58370be0f84e4dc64635d96e25962f1e6e
r.kakaei <r.kakaei@irisaco.com>
2023-12-12 11:53:15 +0330

    add routing for signup component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 
-const routes:Route[] = [
-    {
-        path:'', redirectTo:'signup', pathMatch:'full'
-    },
-    {
-        path:'signup', loadComponent:() => import('./pages/signup/signup.component') .then( c => c.SignupComponent)
-    }
-]
+const routes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'signup',
+    pathMatch: 'full',
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup.component').then((c) => c.SignupComponent),
+  },
+];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
