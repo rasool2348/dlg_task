a68e898da75404c1926fee7942e417123b23ebae
r.kakaei <r.kakaei@irisaco.com>
2023-12-12 11:54:26 +0330

    signup form and error handling + validation + some user interface addition

---
+++ src/app/consts/regexPattern.ts
@@ -0,0 +1,2 @@
+export const NAME_PATTERN = '^[a-z,A_Z]{2,}';
+export const EMAIL_PATTERN = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,13 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { EMAIL_PATTERN, NAME_PATTERN } from 'src/app/consts/regexPattern';
+import { Loading } from 'src/app/models/loading';
+import { User } from 'src/app/models/user';
+import { signupUser } from 'src/app/store/user/user.action';
+import { getLoadingStatus } from 'src/app/store/user/user.selector';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
   standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  constructor(private _store: Store<Loading>, private _fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  userForm = this._fb.group({
+    firstName: this._fb.nonNullable.control('', [
+      Validators.required,
+      Validators.pattern(NAME_PATTERN),
+      Validators.maxLength(40),
+    ]),
+    lastName: this._fb.nonNullable.control('', [
+      Validators.required,
+      Validators.pattern(NAME_PATTERN),
+      Validators.maxLength(40),
+    ]),
+    email: this._fb.nonNullable.control('', [
+      Validators.required,
+      Validators.pattern(EMAIL_PATTERN),
+    ]),
+  });
+
+  ngOnInit(): void {
+    this._store.select(getLoadingStatus).subscribe((data) => console.log(data));
+  }
+
+  submitForm(userForm: typeof this.userForm) {
+    this._store.dispatch(signupUser(userForm.value as User));
+  }
 }
